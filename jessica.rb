dc9a35a521b00400aaf4860100b391589b6cc225
Roman Kotov <waydeveloper@gmail.com>
2018-08-06 22:42:17 +0300

    Update jessica.rb

--- jessica.rb
+++ jessica.rb
@@ -1,8 +1,8 @@
 class Jessica < Formula
   desc "Personal Assistent for iOS project"
   homepage ""
-  url "https://github.com/DaskiOFF/jessica/archive/1.3.3.tar.gz"
-  sha256 "e5f9450f4ff02093f44fcd3feee98ba7882cde5fccc3b936e6b7c85e0dc1fbea"
+  url "https://github.com/DaskiOFF/jessica/archive/1.3.4.tar.gz"
+  sha256 "7986a71e7913241f54e01e2fc00773244280568ea80f598f14bef6ee82c9e414"
   
   depends_on "go@1.9" => :build
   
